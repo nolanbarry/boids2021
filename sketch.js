8e111aba827d0799552a8ded220611bca1010925
Nolan Barry <nolan.barry@live.com>
2021-03-09 15:18:13 -0700

    added obstacle avoidance (just walls for now)

--- sketch.js
+++ sketch.js
@@ -1,12 +1,13 @@
 let boids = [];
 let walls = [];
-let BOID_COUNT = 200;
+let BOID_COUNT = 500;
 let DRAW_WALLS = false;
 let CONTINUE = true;
 let MONITOR = -1;
 let COHESION_MULTIPLIER = 1;
 let ALIGNMENT_MULTIPLIER = 1;
 let SEPARATION_MULTIPLIER = 1;
+let OBSTACLE_POLL_COUNT = 8;
 
 function setup() {
   angleMode(RADIANS);
@@ -20,8 +21,8 @@ let createBoid = (id) => { return {
     y: floor(random(windowHeight)),
     direction: random(2*PI),
     radius: 15,
-    speed: 10,
-    sightRadius: 100,
+    speed: 3,
+    sightRadius: 50,
     id: id,
     color: '#1f1f1f'
   }
@@ -91,6 +92,7 @@ function draw() {
 // advances boid forward one generation
 function advance(boid) {
   let flockmates = boids.filter(cand => dist(boid.x, boid.y, cand.x, cand.y) < boid.sightRadius && !(cand === boid));
+  if (dist(boid.x, boid.y, mouseX, mouseY) < boid.sightRadius) flockmates.push({x: mouseX, y: mouseY, direction: boid.direction, sightRadius: boid.sightRadius});
   // cohesion
   let flockCenter = createPoint(
     flockmates.reduce((a, b) => a + b.x, 0) / flockmates.length - boid.x,
@@ -106,16 +108,22 @@ function advance(boid) {
     flockmates.reduce((a, b) => a - (cos(atan2(b.y - boid.y, b.x - boid.x)) * boid.sightRadius) - (b.x - boid.x), 0) / flockmates.length,
     flockmates.reduce((a, b) => a - (sin(atan2(b.y - boid.y, b.x - boid.x)) * boid.sightRadius) - (b.y - boid.y), 0) / flockmates.length
   );
+  let obstacles = obstacleAvoidance(boid);
   let average = createPoint(
     (flockCenter.x + flockHeading.x + steerAway.x) / 3,
     (flockCenter.y + flockHeading.y + steerAway.y) / 3
   );
+  average.x += obstacles.x / 2;
+  average.y += obstacles.y / 2;
   let dirVect = createPoint(cos(boid.direction), sin(boid.direction));
   dirVect.x += (average.x - dirVect.x) / boid.speed;
   dirVect.y += (average.y - dirVect.y) / boid.speed;
   if (flockmates.length > 0) boid.direction = atan2(dirVect.y, dirVect.x);
   boid.x += cos(boid.direction) * boid.speed;
   boid.y += sin(boid.direction) * boid.speed;
+  boid.x %= windowWidth;
+  boid.y %= windowHeight;
+
   if (boid.id == MONITOR) {
     for(let mate of flockmates) {
       mate.color = '#00ff00';
@@ -124,14 +132,10 @@ function advance(boid) {
     stroke('white');
     circle(boid.x, boid.y, boid.sightRadius * 2);
     noStroke();
-    fill('red')
-    circle(boid.x + flockCenter.x, boid.y + flockCenter.y, 10);
-    fill('blue');
-    circle(boid.x + flockHeading.x, boid.y + flockHeading.y, 10);
-    fill('green');
-    circle(boid.x + steerAway.x, boid.y + steerAway.y, 10);
-    fill('black');
-    circle(boid.x + average.x, boid.y + average.y, 10);
+    fill('red');    circle(boid.x + flockCenter.x, boid.y + flockCenter.y, 10);
+    fill('blue');   circle(boid.x + flockHeading.x, boid.y + flockHeading.y, 10);
+    fill('green');  circle(boid.x + steerAway.x, boid.y + steerAway.y, 10);
+    fill('black');  circle(boid.x + average.x, boid.y + average.y, 10);
     // stroke('green');
     // draw steer away markers
     // flockmates.forEach((b) => {
@@ -150,3 +154,57 @@ function heading(boid) {
     boid.y + sin(boid.direction) * boid.sightRadius
   );
 }
+
+function obstacleAvoidance(boid) {
+  let polls = [];
+  for(let i = 0; i < OBSTACLE_POLL_COUNT; ++i) {
+    polls.push({
+      p1: createPoint(boid.x, boid.y),
+      p2: createPoint(
+        boid.x + cos(boid.direction + 2*PI*(i/OBSTACLE_POLL_COUNT)) * boid.sightRadius,
+        boid.y + sin(boid.direction + 2*PI*(i/OBSTACLE_POLL_COUNT)) * boid.sightRadius
+      )
+    })
+  }
+  let obstacles = []
+  for(let l of polls) {
+    for (let wall of walls) {
+      let inter = intersectionOf(l, wall);
+      if (inter != null) {
+        obstacles.push(inter);
+      }
+    }
+    if (boid.id == MONITOR) {
+      stroke('#000000');
+      line(l.p1.x, l.p1.y, l.p2.x, l.p2.y);
+    }
+  }
+  if (obstacles.length > 0) {
+    let avoid = createPoint(
+      obstacles.reduce((a, b) => a - pow(cos(atan2(b.y - boid.y, b.x - boid.x)) * boid.sightRadius - (b.x - boid.x), 1), 0) / obstacles.length,
+      obstacles.reduce((a, b) => a - pow(sin(atan2(b.y - boid.y, b.x - boid.x)) * boid.sightRadius - (b.y - boid.y), 1), 0) / obstacles.length
+    );
+    return avoid;
+  }
+  return createPoint(0, 0);
+}
+
+function intersectionOf(l1, l2) {
+  return intersects(l1.p1.x, l1.p1.y, l1.p2.x, l1.p2.y, l2.p1.x, l2.p1.y, l2.p2.x, l2.p2.y);
+}
+
+// returns true if the line from (a,b)->(c,d) intersects with (p,q)->(r,s)
+// https://stackoverflow.com/questions/9043805/test-if-two-lines-intersect-javascript-function
+function intersects(a,b,c,d,p,q,r,s) {
+  var det, gamma, lambda;
+  det = (c - a) * (s - q) - (r - p) * (d - b);
+  if (det === 0) {
+    return false;
+  } else {
+    lambda = ((s - q) * (r - a) + (p - r) * (s - b)) / det;
+    gamma = ((b - d) * (r - a) + (c - a) * (s - b)) / det;
+    let result = (0 < lambda && lambda < 1) && (0 < gamma && gamma < 1);
+    if (result) return createPoint(a + lambda*(c-a), b + lambda*(d-b))
+    else return null;
+  }
+};
